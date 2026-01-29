a43192c76fb748084cc708915689f45170167673
Shrirkishba <123809369+Krishna7420@users.noreply.github.com>
2026-01-29 14:56:46 +0530

    Initial Commit

---
+++ Firebase_Authentication_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Authentication_
+//
+//  Created by Shrikrishna Thodsare on 29/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
